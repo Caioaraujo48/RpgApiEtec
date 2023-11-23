98f93bc028ff43c33728acb5cd10350c57eb0316
Etec Prof. Horácio Augusto da Silveira <caioquir@gmail.com>
2023-11-22 21:22:57 -0300

    Disputas feitas

---
+++ AppRpgEtec/ViewModels/Disputas/DisputaViewModel.cs
@@ -0,0 +1,100 @@
+﻿using AppRpgEtec.Models;
+using AppRpgEtec.Services.Personagens;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AppRpgEtec.ViewModels.Disputas
+{
+    public class DisputaViewModel : BaseViewModel
+    {
+        private PersonagemService pService;
+
+        public ObservableCollection<Personagem> PersonagensEncontrados { get; set; }
+
+        public Personagem Atacante { get; set; }
+
+        public Personagem Oponente { get; set; }
+
+        public ICommand PesquisarPersonagensCommand; 
+
+        public DisputaViewModel()
+        {
+            string token = Preferences.Get("UsuarioToken", string.Empty);
+            pService = new PersonagemService(token);
+
+            Atacante = new Personagem();
+            Oponente = new Personagem();
+
+            PesquisarPersonagensCommand =
+                new Command<string>(async (string pesquisa) => { await PesquisarPersonagens(pesquisa); });
+        }
+
+        public async Task PesquisarPersonagens (string textoPesquisaPersonagem)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex) 
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+
+        public string DescricaoPersonagemAtacante
+        {
+            get => Atacante.Nome;
+        }
+
+        public string DescricaoPersonagemOponente
+        {
+            get => Atacante.Nome;
+        }
+
+        public async void SelecionarPersonagem (Personagem p)
+        {
+            try
+            {
+                string tipoCombatente = await Application.Current.MainPage
+                    .DisplayActionSheet("Atacante ou Oponente?", "Cancelar", "", "Atacante", "Oponente");
+
+                if (tipoCombatente == "Atacante")
+                {
+                    Atacante = p;
+                    OnPropertyChanged(nameof(DescricaoPersonagemAtacante));
+                }
+                else if (tipoCombatente == "Oponente")
+                {
+                    Oponente = p;
+                    OnPropertyChanged(nameof(DescricaoPersonagemOponente));
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+        private Personagem personagemSelecionado;
+
+        public Personagem PersonagemSelecionado
+        {
+            set
+            {
+                if (value == null)
+                {
+                    personagemSelecionado = value;
+                    SelecionarPersonagem(personagemSelecionado);
+                    OnPropertyChanged();
+                    PersonagensEncontrados.Clear();
+                }
+            }
+        }
+    }
+}
